3beae3283e0c6f8e869922efe0aa4cf79d4cb1cc
SeokSeungMin <seoksmin@gmail.com>
2024-07-31 15:30:28 +0900

    testCode, Jacoco Test

--- src/main/java/com/springboot/jpa/data/dao/ProductDAO.java
+++ src/main/java/com/springboot/jpa/data/dao/ProductDAO.java
@@ -1,8 +1,6 @@
 package com.springboot.jpa.data.dao;
 
 import com.springboot.jpa.data.entity.Product;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
@@ -10,10 +8,7 @@ import java.util.Optional;
 public interface ProductDAO {
 
     Product insertProduct(Product product);
-
     Optional<Product> selectProduct(Long number);
-
     Product updateProductName(Long number, String namem) throws Exception;
-
     void deleteProduct(Long number) throws Exception;
 }
